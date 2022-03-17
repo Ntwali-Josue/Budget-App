3d96641662287856835622525c2f891ebcdeb39e
KARANGWA Ntwali Josue <kntwalijos@gmail.com>
2022-03-17 17:24:26 +0200

    Fix signup page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :update_allowed_parameters, if: :devise_controller?
 
+
   def after_sign_in_path_for(_resource)
+    root_path
+  end
+
+  def after_sign_up_path_for(_resource)
     categories_path
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
-  resources :entities
-  resources :categories
   
   devise_for :users
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
   root "home#index"
+  resources :categories
+  resources :entities
+
 end
